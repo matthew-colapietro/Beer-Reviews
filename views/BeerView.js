9af165e6338ad7c947b86cd306226887086edc57
MCola <mcolapietro502@gmail.com>
2020-05-20 22:17:15 -0400

    completed edit button and edit functionality in BeerView.js

--- views/BeerView.js
+++ views/BeerView.js
@@ -2,6 +2,32 @@ var BeerView = Backbone.View.extend({
 
   className: 'beer',
 
+  events: {
+    'dblclick .edit': 'editBeer',
+    'click .remove': 'delete',
+    'keypress .edit-mode': 'updateText'
+  },
+
+  editBeer: function () {
+    this.$('input').toggle();
+  },
+
+  updateText: function(e) {
+			if (e.which === 13) {
+				this.model.set('name', this.$('input').val())
+			}
+  },
+
+  delete: function () {
+    this.model.destroy();
+    console.log('destroyed')
+  },
+
+  initialize: function() {
+    this.listenTo(this.model, 'destroy', this.remove)
+    this.listenTo(this.model, 'change', this.render);
+  },
+
   template: Handlebars.compile($('#beer-template').html()),
 
   render: function() {
